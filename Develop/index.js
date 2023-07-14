fcb8bf44f1a7a8f859f80c336820d80feb654cd8
Tristenh <tristenhobba1@gmail.com>
2023-07-14 21:20:57 +1000

    create function to write README file, change license question to list type

--- Develop/index.js
+++ Develop/index.js
@@ -2,60 +2,66 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 // TODO: Create an array of questions for user input
-inquirer.prompt([
-  {
-    type: "input",
-    name: "title",
-    message: "your project name",
-  },
-  {
-    type: "input",
-    name: "description",
-    message:
-      "Provide a short description explaining the what, why, and how of your project",
-  },
-  {
-    type: "input",
-    name: "table of contents",
-    message: "list your table of contents (optional)",
-  },
-  {
-    type: "input",
-    name: "installation",
-    message: "What are the steps required to install your project?",
-  },
-  {
-    type: "input",
-    name: "usage",
-    message: "Provide instructions and examples for use",
-  },
-  {
-    type: "checkbox",
-    name: "license",
-    message: "any applcable license, choose from the provided list",
-    choices: ["MIT", "GPLv3", "BSD", "Apache 2.0"],
-  },
-  {
-    type: "input",
-    name: "contributing",
-    message:
-      "List your collaborators, if any, with links to their GitHub profiles",
-  },
-  {
-    type: "input",
-    name: "tests",
-    message:
-      "write tests for your application. Then provide examples on how to run them here",
-  },
-  {
-    type: "input",
-    name: "questions",
-    message: "enter git hub username and email",
-  },
-]);
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "title",
+      message: "your project name",
+    },
+    {
+      type: "input",
+      name: "description",
+      message:
+        "Provide a short description explaining the what, why, and how of your project",
+    },
+    {
+      type: "input",
+      name: "table of contents",
+      message: "list your table of contents (optional)",
+    },
+    {
+      type: "input",
+      name: "installation",
+      message: "What are the steps required to install your project?",
+    },
+    {
+      type: "input",
+      name: "usage",
+      message: "Provide instructions and examples for use",
+    },
+    {
+      type: "list",
+      name: "license",
+      message: "any applcable license, choose from the provided list",
+      choices: ["MIT", "GPLv3", "BSD", "Apache 2.0"],
+    },
+    {
+      type: "input",
+      name: "contributing",
+      message:
+        "List your collaborators, if any, with links to their GitHub profiles",
+    },
+    {
+      type: "input",
+      name: "tests",
+      message:
+        "write tests for your application. Then provide examples on how to run them here",
+    },
+    {
+      type: "input",
+      name: "questions",
+      message: "enter git hub username and email",
+    },
+  ])
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+  // TODO: Create a function to write README file
+  .then((data) => {
+    const filename = "README.md";
+    fs.writeFile(filename, JSON.stringify(data, null, `\t`), (err) =>
+      err ? console.log(err) : console.log(`success!`)
+    );
+  });
 
 // TODO: Create a function to initialize app
 function init() {}
